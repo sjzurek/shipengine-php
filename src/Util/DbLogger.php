13f03995f2ce3238dcb9d8031ddaf91355a2e028
simon zurek <sjzurek@gmail.com>
2025-11-07 17:29:42 +0100

    Disable logging for test

--- src/Util/DbLogger.php
+++ src/Util/DbLogger.php
@@ -6,6 +6,7 @@ final class DbLogger
 {
     public static function log(array $values): bool
     {
+        return true;
         return (bool) \pg_insert(
             \pg_connect($_ENV['PG_LOGS']            ),
             'psr7s_logs',
